062cdf6f54ac4b8275de3d0eff5a6a10b4ff5931
saivarshithnaidu <saivarshith8284@gmail.com>
2026-03-10 17:45:54 +0530

    Fix StudyForge PDF worker by pinning local pdfjs-dist version

--- components/studyforge/PDFViewer.tsx
+++ components/studyforge/PDFViewer.tsx
@@ -9,7 +9,7 @@ import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 
 // Set worker
-pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = '/pdfjs-worker/pdf.worker.min.mjs';
 
 interface PDFViewerProps {
     fileUrl: string;
